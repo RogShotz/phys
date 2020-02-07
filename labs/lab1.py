44ce0eb76f4355477215fdfeccf23b2fe74eb5da
Shaddox0 <60760610+Shaddox0@users.noreply.github.com>
2020-02-07 15:55:42 -0800

    Update lab1.py
    This is the copy from the one I was working on in the class.

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,53 +1,64 @@
 from visual import *
-#I changed the code for 1d motion. Jorstad said we are over complicating it.
-
-#====Notes=====
-#When Tao is 2.1 the ball will keep going up but very slowly
-#~ a_init = 10
-#~ v_init = < 0, 0>
-#find terminal velocity
-
-#Creating backgroud plane (boxa object)
-background = box(size = vector(2.25, 2.25, .01),
+#Creating backgroud plane (box object)
+background = box(size = vector(4.5, 4.5, .01),
                   color = color.blue)
 
-offset = .5
-pos_init = vector(0, 1)
-
-ball = sphere (pos = pos_init, color = color.white, radius = 0.05)
-#line_y.x = line_y.x - ball.radius * 3  #Moved the line over to the left
+offset = 1
+pos_init = vector(0, 2)
+ball = sphere (pos = pos_init, color = color.white, radius = 0.1)
 velInit = vector(0, 0)
 ball.vel = velInit
 
 #creating Lines
-#line_y = cylinder(pos = vector(pos_init.x - 0.1, -offset), axis= (0, 1), radius =.01, color = color.black)
-line_x = cylinder(pos = vector(-offset, -offset), axis= (2 * offset, 0), radius =.01, color = color.black)
+line_y1 = cylinder(pos = vector(pos_init.x - 2*ball.radius, -offset),
+                   axis= (0, offset),
+                   radius =.01,
+                   color = color.red)
+line_y2 = cylinder(pos = vector(pos_init.x - 2*ball.radius, -offset + offset),
+                   axis= (0, offset),
+                   radius =.01,
+                   color = color.blue)
+line_y3 = cylinder(pos = vector(pos_init.x - 2*ball.radius, -offset + 2 * offset),
+                   axis= (0, offset),
+                   radius =.01,
+                   color = color.green)
+line_x = cylinder(pos = vector(-offset, -offset - .01),
+                  axis= (2 * offset, 0),
+                  radius =.01,
+                  color = color.black)
 
-#arrow (pos=ball.pos, axis = velInit*.1, color = color.green)
-deltat = .05
+deltat = .01
 t = 0
 a = vector(0,-10)
-Tao = .1               #only seems to have a noticable change between # and #
+Tao = .1                #only seems to have a noticable change between # and #
 inc = .1                #the amount we will subract from Tao for each iteration
 yDefault = line_x.y + line_x.radius + ball.radius   #default y position that nothing should go below
-simSpeed = 1            #Controlls the speed of the simulation
 replay = false          #restarts the animation(resets the ball position to origin)
 inputDriven = false     #allows for input from user
+simActive = true        #Helps to stop while loop when the ball is at the bottem
+simSpeed = 1
 
-while ball.pos.y >= yDefault:
+while ball.pos.y >= yDefault and simActive == true:
     rate(simSpeed/deltat)
-    print("Y Position:% .3f, Time:% .3f, Tao:% .3f" %(ball.pos.y + offset,
+    print("Y Position:% .3f, Time:% .3f, Tao:% .3f" %((ball.pos.y - line_x.pos.y + line_x.radius),
                                                     t,
                                                     Tao))
-    a = a * math.e**(-t/Tao)
-    ball.vel = (ball.vel + a*deltat)
 
-    #this makes sure it does go below the line
+    #Updates the balls 'ACCELERATION'
+    a = a * math.e**(-t/Tao)
+
+    #Updates the balls 'VELOCITY'
+    ball.vel = (ball.vel + a*deltat)
+    
+    #Updates the balls 'POSITION' and makes sure it does not go below the line
     if ball.pos.y + ball.vel.y*deltat < yDefault:
         ball.pos.y = yDefault
+        if replay == false:
+            simActive = false
     else:
         ball.pos = (ball.pos+ball.vel*deltat)
-        
+
+    #Updates 'TIME'
     t = t + deltat
 
 
@@ -67,8 +78,7 @@ while ball.pos.y >= yDefault:
             else:
                 Tao = userInput
         else: #Incrementing Tao
-            Tao = Tao - inc
+            Tao = Tao - 0
         
         
         
-
