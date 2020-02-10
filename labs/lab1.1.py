1674ef39df44b743f8b986fb8e35d46e6f42a5c3
Luke Rowe <RogShotz@users.noreply.github.com>
2020-02-10 13:19:07 -0800

    Update lab1.1.py
    Fix starting at 1 + the y we want
    Reset axis's (broke now though)
    Change default Y
    Change tao to mimic time (not vel)
    Change all prev values to init values

--- labs/lab1.1.py
+++ labs/lab1.1.py
@@ -5,27 +5,27 @@ background = box(size = vector(4.5, 4.5, .01),
                   color = color.blue)
 
 
-#Simpulation Variables
+#Simulation Variables
 offset = 1
-pos_init = vector(0, 2)
-ball = sphere (pos = vector(0, 1), color = color.white, radius = 0.1)
+posInit = vector(0, 2) #radius
+ball = sphere (pos = posInit, color = color.white, radius = 0.1)
 velInit = vector(0, 0)
 ball.vel = velInit
 
 #creating Lines
-line_y1 = cylinder(pos = vector(pos_init.x - 2*ball.radius, -offset),
+line_y1 = cylinder(pos = vector(posInit.x - 2*ball.radius, -offset),
                    axis= (0, offset),
                    radius =.01,
                    color = color.red)
-line_y2 = cylinder(pos = vector(pos_init.x - 2*ball.radius, -offset + offset),
+line_y2 = cylinder(pos = vector(posInit.x - 2*ball.radius, -offset + offset),
                    axis= (0, offset),
                    radius =.01,
                    color = color.blue)
-line_y3 = cylinder(pos = vector(pos_init.x - 2*ball.radius, -offset + 2 * offset),
+line_y3 = cylinder(pos = vector(posInit.x - 2*ball.radius, -offset + 2 * offset),
                    axis= (0, offset),
                    radius =.01,
                    color = color.green)
-line_x = cylinder(pos = vector(-offset, -offset - .01),
+line_x = cylinder(pos = vector(-offset, 0),
                   axis= (2 * offset, 0),
                   radius =.01,
                   color = color.black)
@@ -34,19 +34,19 @@ deltat = .01
 t = 0
 a_0 = vector(0 , -9.8)
 a = vector(0, -9.8)
-Tao = 100           #only seems to have a noticable change between # and #
-inc = .1                #the amount we will subract from Tao for each iteration
-yDefault = line_x.y + line_x.radius + ball.radius   #default y position that nothing should go below
+Tao = -30               #only seems to have a noticable change between # and #
+inc = 1                 #the amount we will subract from Tao for each iteration
+yDefault = line_x.radius #default y position that nothing should go below
 replay = false          #restarts the animation(resets the ball position to origin)
 inputDriven = false     #allows for input from user
 simActive = true        #Helps to stop while loop when the ball is at the bottem
-simSpeed = .01
+simSpeed = .5
 
 while ball.pos.y >= yDefault and simActive == true:
     rate(simSpeed/deltat)
 
     #print to screen
-    print("Y:% .3f, t:% .3f, T:% .3f, a: % .3f ,v: % .3f" %((ball.pos.y - line_x.pos.y + line_x.radius),
+    print("Y:% .3f, t:% .3f, T:% .3f, a: % .3f ,v: % .3f" %((ball.pos.y),
                                                     t,
                                                     Tao,
                                                     a.y,
@@ -56,15 +56,15 @@ while ball.pos.y >= yDefault and simActive == true:
     a = a_0 * math.e**(-t/Tao)
 
     #Updates the balls 'VELOCITY'
-    ball.vel = (ball.vel - (Tao * a * (math.e**(-t/Tao) - 1)))
+    ball.vel = (velInit - (Tao * a * (math.e**(-t/Tao) - 1)))
     
     #Updates the balls 'POSITION' and makes sure it does not go below the line
-    if ball.pos.y + ball.vel.y*deltat < yDefault:
+    if (posInit + velInit * t + (a * Tao * (Tao * math.e**(-t/Tao) + t - Tao))) < yDefault:
         ball.pos.y = yDefault
         if replay == false:
             simActive = false
     else:
-        ball.pos = (ball.pos + ball.vel * t + (a * Tao * (Tao * math.e**(-t/Tao) + t - Tao)))
+        ball.pos = (posInit + velInit * t + (a * Tao * (Tao * math.e**(-t/Tao) + t - Tao)))
 
     #Updates 'TIME'
     t = t + deltat
@@ -72,7 +72,7 @@ while ball.pos.y >= yDefault and simActive == true:
 
     if replay == true and ball.pos.y == yDefault:        
         #reset ball information
-        ball.pos = pos_init
+        ball.pos = posInit
         ball.vel = velInit
         ball.pos.y = 0
         t = 0
