691356ea33cfa06a8750255b79888d2c30d3d4f7
Luke Rowe <RogShotz@users.noreply.github.com>
2020-11-02 16:35:45 -0800

    update lab

--- phys163labs/lab4 pres/lab4postestob.py
+++ phys163labs/lab4 pres/lab4postestob.py
@@ -18,13 +18,14 @@ dtheta = 2 * pi / n  # spacing between adjacent balls in rod
 ring = []  # creates empty list
 
 # Variable Initializations
-i = 8e-3  # current
+i = 1  # current
 c = 5  # number of current arrows
 B_tot = vec(0, 0, 0)
 B = vec(0, 0, 0)
 p = vec(0, 0, 0)
 z = 0
-mu_o = 4 * pi * 10e-7
+mu_0 = 4 * pi * 1e-7
+k_bio = mu_0 * i / (4 * pi)
 
 # POI Initializations
 POI = vec(0, 0, -.2)
@@ -50,7 +51,7 @@ def ring_gen(z_pos, rad):  # creates rings
         r = POI - (segment.pos + 0.5 * segment.axis * scale)
 
         ds = segment.axis * scale
-        B_func += i * cross(ds, r) / mag(r) ** 3
+        B_func += k_bio * cross(ds, r) / mag(r) ** 3
     return B_func
 
 
@@ -60,7 +61,7 @@ def get_b(POI):  # gets b_ext from any POI
         r = POI - (ring[j].pos + 0.5 * ring[j].axis * scale)
 
         ds = ring[j].axis * scale
-        B_new += i * mu_o * cross(ds, r) / mag(r) ** 3
+        B_new += k_bio * cross(ds, r) / mag(r) ** 3
     return B_new
 
 
@@ -74,25 +75,34 @@ B_tot += ring_gen(ring4pos, in_rad)
 B_tot += ring_gen(ring5pos, in_rad)
 
 slope_range = abs(ring1pos) + 5  # tweak var at end to expand or shrink slope field range of b arrows
-
+field_max = 1.5e-11
 for y in range(-slope_range + 5, slope_range - 5):
     for x in range(-slope_range, slope_range):
         fieldPOI = vec(x, y, z)
         field = get_b(fieldPOI)
-        field_arrow = arrow(pos=fieldPOI, axis=hat(field), color=color.green)
+        field_arrow = arrow(pos=fieldPOI, axis=hat(field))
+
+        if mag(field) >= 0:
+            field_arrow.color = color.green
+        else:
+            field_arrow.color = color.blue
+        if mag(field) > field_max:
+            field_arrow.opacity = 1
+        if mag(field) < -field_max:
+            field_arrow.opacity = 1
+        else:
+            field_arrow.opacity = mag(field) / field_max
 
 # Animations Initialization
 t = 0
 dt = .01
 simspeed = 10000
 
-while t < 8000:
+while t < 8000 and mag(ball.pos) * 0.8 < ring2pos:
     rate(simspeed / dt)
     B = get_b(ball.pos)
     F = ball.q * cross(ball.v, B)
     print(mag(B))
     ball.v += F * dt / ball.m
     ball.pos += ball.v * dt
-    if mag(ball.pos) * 0.8 > ring2pos:  # stops the loops so it doesnt zoom out because of a launched particle
-        break
     t += dt
